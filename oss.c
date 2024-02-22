59a95f21f775112f6ad067827ce62d6d213f3a17
ajsgcz <160422427+ajsgcz@users.noreply.github.com>
2024-02-22 11:45:30 -0600

    Update oss.c

--- oss.c
+++ oss.c
@@ -5,13 +5,24 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <sys/sem.h>
+#include <signal.h>
 
 #define SHM_KEY_CLOCK 1234
 #define SHM_KEY_PROCESS_TABLE 5678
 #define SEM_KEY 9876
 #define MAX_CHILDREN 20
+#define SHMEM_KEY 12345
+#define MAX_PROCESSES 20
+#define SIGALRM 14
 
-struct PCB {
+struct Clock
+{
+    int seconds;
+    int nanoseconds;
+};
+
+struct PCB
+{
     int occupied;
     pid_t pid;
     int startSeconds;
@@ -22,43 +33,101 @@ int shmid_clock;
 int shmid_process_table;
 int semid;
 
-void initializeResources() {
-    // Initialize shared memory for clock and process table
-    // Initialize semaphores
-}
+struct PCB processTable[MAX_CHILDREN];
+int stillChildrenToLaunch = 1;
 
-void cleanupResources() {
-    // Release shared memory
-    // Release semaphores
-}
+void timeout_handler(int signal_num);
+void ctrlc_handler(int sig);
+void launch_child_processes(int num_children);
+void wait_for_child_processes();
 
-void updateClock() {
-    // Update simulated system clock
-}
+pid_t child_pids[MAX_CHILDREN];
+int num_children = 0;
 
-void launchWorkerProcess() {
-    // Fork and exec worker process
-    // Update process table
-}
+void incrementClock();
+void outputProcessTable();
+void checkIfChildHasTerminated();
+void updatePCBOfTerminatedChild();
+void possiblyLaunchNewChild();
 
-void handleProcessTermination() {
-    // Detect and handle termination of worker processes
-}
+int main()
+{
+    signal(SIGALRM, timeout_handler);
+    signal(SIGINT, ctrlc_handler);
 
-int main() {
-    // Initialization
-    initializeResources();
+    alarm(60);
 
-    // Main loop
-    while (/* condition */) {
-        // Check process table for free slots and launch new processes if possible
-        // Check for termination of worker processes
-        // Update system clock
+    while (1)
+    {
+
+        if (num_children < MAX_CHILDREN)
+        {
+            launch_child_processes(1);
+        }
+
+        wait_for_child_processes();
     }
 
-    // Cleanup
-    cleanupResources();
+    while (stillChildrenToLaunch)
+    {
 
-    return 0;
+        incrementClock();
+
+        outputProcessTable();
+
+        checkIfChildHasTerminated();
+
+        possiblyLaunchNewChild();
+
+        return 0;
+    }
 }
 
+void incrementClock()
+{
+}
+
+void outputProcessTable()
+{
+}
+
+void checkIfChildHasTerminated()
+{
+    int status;
+    pid_t pid = waitpid(-1, &status, WNOHANG);
+    if (pid > 0)
+    {
+
+        updatePCBOfTerminatedChild();
+    }
+}
+
+void updatePCBOfTerminatedChild()
+{
+}
+
+void possiblyLaunchNewChild()
+{
+}
+
+void timeout_handler(int signal_num)
+{
+
+    for (int i = 0; i < num_children; i++)
+    {
+        kill(child_pids[i], SIGTERM);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+void ctrlc_handler(int sig)
+{
+
+    for (int i = 0; i < num_children; i++)
+    {
+        kill(child_pids[i], SIGTERM);
+    }
+
+    exit(EXIT_SUCCESS);
+}
